186f7db2b3c4ea6f05be4b49a45783e365745cc6
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2020-01-09 02:11:09 +0200

    Print session vars services

--- app/services.php
+++ app/services.php
@@ -2,14 +2,17 @@
 require_once '../app/twig.php';
 
 session_start();
+print_r($_SESSION);
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
         //header("location: welcome.php");
+
         echo $twig->render('services.html', ['name' => $_SESSION["username"], 'home' => "nav-item", 
         'services' => "nav-item active", 
         'contactus' => "nav-item", 
         'aboutus' => "nav-item",
         'login' => "nav-item"] ); 
+
         exit;
 }
 echo $twig->render('services.html', ['home' => "nav-item", 
