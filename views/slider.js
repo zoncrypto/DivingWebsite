384c17d7cb633454ed9014e745b50772aafe207c
unknown <alexandra@alexandermooresa.com>
2020-01-17 20:14:55 +0200

    Contact form

--- views/slider.js
+++ views/slider.js
@@ -14,7 +14,7 @@ function currentSlide(n) {
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
+    var dots = document.getElementsByClassName("cursor");
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
